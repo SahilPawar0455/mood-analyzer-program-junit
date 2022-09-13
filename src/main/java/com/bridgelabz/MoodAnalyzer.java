8baa97757f7f44b7a1c240cfd15acac933b3545f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-13 19:49:15 +0530

    ignore file

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+    static String mood;
+
+    public String analyseMood(String message) {
+        if (message.equals("I am in Sad Mood")) {
+            return mood = "sad";
+        } else {
+            return mood = "Happy";
+        }}
+    public static void main(String[] args) {
+        System.out.println("Welcome the Mood Analyzer Program");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        System.out.println("Current mood is "+moodAnalyzer.analyseMood("I am in Sad Mood"));
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMessageSadThanMoodAnalyzerSad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String actualMood1 = moodAnalyzer.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("sad",actualMood1);
+    }
+    @Test
+    public void  givenMessageAnyMoodThenMoodAnalyzerHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String actualMood2 = moodAnalyzer.analyseMood("I am in Any Mood");
+        Assert.assertEquals("Happy",actualMood2);
+    }
+}
