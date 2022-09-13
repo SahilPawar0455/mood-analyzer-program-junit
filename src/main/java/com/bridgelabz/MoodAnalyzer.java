422847edf6483f8561834823a31e7f2f0a99446f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-13 19:52:18 +0530

    ADDED: Given massages , ability to analyse and respond happy or sad mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -8,10 +8,12 @@ public class MoodAnalyzer {
             return mood = "sad";
         } else {
             return mood = "Happy";
-        }}
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome the Mood Analyzer Program");
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        System.out.println("Current mood is "+moodAnalyzer.analyseMood("I am in Sad Mood"));
+        System.out.println("Current mood is " + moodAnalyzer.analyseMood("I am in Sad Mood"));
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -5,15 +5,16 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessageSadThanMoodAnalyzerSad(){
+    public void givenMessageSadThanMoodAnalyzerSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String actualMood1 = moodAnalyzer.analyseMood("I am in Sad Mood");
-        Assert.assertEquals("sad",actualMood1);
+        Assert.assertEquals("sad", actualMood1);
     }
+
     @Test
-    public void  givenMessageAnyMoodThenMoodAnalyzerHappy(){
+    public void givenMessageAnyMoodThenMoodAnalyzerHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String actualMood2 = moodAnalyzer.analyseMood("I am in Any Mood");
-        Assert.assertEquals("Happy",actualMood2);
+        Assert.assertEquals("Happy", actualMood2);
     }
 }
